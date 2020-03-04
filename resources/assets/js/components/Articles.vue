9fb875ebd7ab6d9033e2c7ebafe0cdc13a218752
h6106600 <h6106600@gmail.com>
2020-03-04 15:51:09 +0800

    1

---
+++ resources/assets/js/components/Articles.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container">
+        <div>
+            <h2>SCSS</h2>
+            <p :class="$style.red">message：{{ message }}</p>
+            <p :id="$style.gray">now (computed)：{{ now }}</p>
+            <p>getNow (method)：{{ getNow() }}</p>
+            <button @click="test">asdasd</button>
+        </div>
+    </div>
+</template>
+
+<style module>
+    .red {
+        color: red;
+    }
+    #gray{
+        color: gray;
+    }
+</style>
+
+<script>
+    export default {
+        data(){
+            return{
+                message:'123456'
+            }
+        },
+        computed: {
+            now: function() {
+                return Date.now();
+            }
+        },
+        methods: {
+            getNow: function() {
+                return Date.now();
+            },
+            test(){
+                alert(123123);
+            }
+        }
+    }
+</script>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>123</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+<body>
+
+<div id="app">
+    <articles></articles>
+</div>
+
+<script src="{{asset('js/app.js')}}"></script>
+
+</body>
+</html>
